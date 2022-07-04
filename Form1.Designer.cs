a648fbadc101110c0988d5bfcde133aef9ec3cfc
GeorgeYell <Zhegor89@gmail.com>
2022-07-04 18:01:53 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace PBD_KR1
+{
+    public partial class Form1 : Form
+    {
+
+        static string host = "127.0.0.1"; // Имя хоста
+        static string database = "PBD"; // Имя базы данных
+        static string user = "root"; // Имя пользователя
+        static string password = "wray9nus"; // Пароль пользователя
+        static string Connect = "Database=" + database + ";Datasource=" + host + ";User=" + user + ";Password=" + password;
+        MySqlConnection cnn = new MySqlConnection(Connect);
+
+        DataSet ds = new DataSet();
+        BindingSource bindGraph = new BindingSource();
+        BindingSource bindVertex = new BindingSource();
+        MySqlDataAdapter daGraph = new MySqlDataAdapter();
+        MySqlDataAdapter daVertex = new MySqlDataAdapter();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // bind Vertex
+            daVertex.SelectCommand = new MySqlCommand("select * from VERTEX", cnn);
+            daVertex.Fill(ds, "VERTEX");
+            bindVertex.DataSource = ds.Tables["VERTEX"];
+            dataGridView2.DataSource = bindVertex;
+
+            // bind Graphs
+            daGraph.SelectCommand = new MySqlCommand("select * from GRAPH", cnn);
+            daGraph.Fill(ds, "GRAPH");
+            bindGraph.DataSource = ds.Tables["GRAPH"];
+            dataGridView1.DataSource = bindGraph;
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            bindVertex.Filter = "GraphGid = '" + dataGridView1.CurrentRow.Cells["Gid"].Value + "'";
+            using (MySqlConnection conn = new MySqlConnection(Connect))
+            {
+                conn.Open();
+                int id = (int) dataGridView1.CurrentRow.Cells["AuthorGid"].Value;
+                string id_str = id.ToString();
+
+                MySqlCommand cmd = new MySqlCommand("SELECT LastName FROM AUTHOR WHERE Gid = " + id_str, conn);
+                using (MySqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        label3.Text = "Автор: " + (string)dr["LastName"];
+                    }
+                    dr.Close();
+                }
+                conn.Close();
+            }
+        }
+        private DataGridView dataGridView1;
+        private DataGridView dataGridView2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PBD_KR1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 39);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(458, 313);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(487, 39);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(460, 313);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(178, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Графы";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(693, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Вершины";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 369);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Выбранный автор:";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(959, 401);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+    }
+}
